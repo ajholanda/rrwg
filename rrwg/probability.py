88733ec0b330e39dd4027928f6bdd15964efe165
Adriano J. Holanda <aholanda@usp.br>
2022-06-24 15:50:56 -0300

    probability transition calculation functions

--- rrwg/probability.py
+++ rrwg/probability.py
@@ -1,90 +1,115 @@
+"""This module is used in the RRWG simulation to calculate the
+probability of a walk w located in a vertex u to go to a neighbor v of
+u, taking into account the number of visits of the other walks in the
+vertex v. The walk w tends to go to the vertex less visited by the
+other walks.
+
+"""
+import math
+import sys
+
+from rrwg.walks import Walks
+
+def vertex_count_visits(walks: Walks, vert: int) -> int:
+    """Count the number of visits of all walks in the specified vertex.
+
+    walks (Walks): placeholder object for all walks
+    vert (int): vertex to count the visits
+
+    """
+    return walks.count_vertex_visits(vert)
+
+def sum_norm_vertex_visits_from_other_walks(walks: Walks, \
+                                            cur_walk: int,
+                                            vert: int) -> float:
+    """Sum of the normalized number of visits from all walks
+    excepting the current walk to the specified vertex.
+
+    walks (Walks): placeholder for the walk objects
+    cur_walk (int): index of the current walk
+    vert (int): vertex eligible to be visited
+
+    """
+    # Accumulator of the normalized number of visits from the
+    # walks, excepting the current walk.
+    acc = 0.0
+
+    # Total number of visits in the vertex vert
+    total_nvis = vertex_count_visits(walks, vert)
+
+    for walk in range(len(walks)):
+        if walk == cur_walk: # ignore current walk
+            continue
+
+        norm_nvis = \
+            float(walks.get(walk).nvisits(vert)) / total_nvis
+        acc += norm_nvis
+
+    return acc
 
 class Probability():
     """Transition probability class.
 
     """
-    def __init__(self, rrwg, alpha: float, function='EXP'):
+    def __init__(self, alpha: float, function='EXP'):
         """Initialize probability object with the reinforced factor alpha and
         the function chosen to be used in the calculation.
 
         """
-        self._rrwg = rrwg
         self._alpha = alpha
-        if func == 'EXP':
+        if function == 'EXP':
             self.__func = self.__exp
-        elif func == 'POW':
+        elif function == 'POW':
             self.__func = self.__pow
         else:
-            sys.exit('panic: unknown function \"{}\"'.format(func))
+            sys.exit('panic: unknown function \"{}\"'.format(function))
 
-    
-    def __exp(self, cur_walk: int, vert: int):
+
+    def __exp(self, walks: Walks, cur_walk: int, vert: int):
         """Apply the exponential function to the number of visits and the
         reinforcing factor alpha.
 
         """
-        pr_w = 0.0
-        acc = 0.0
+        acc = \
+            sum_norm_vertex_visits_from_other_walks(walks, cur_walk, vert)
 
-        # Total number of visits in the vertex vert
-        total_nvis = self._walks.count_vertex_visits(vert)
+        prob = math.exp(-self._alpha * acc)
 
-        for walk in range(self._m):
-            norm_nvis = \
-                self._walks.get(walk).nvisits(vert) / total_nvis
+        # TODO: put print() into log file
+        #print('\t\tEXP: w{}, v{}={}'.format(cur_walk, vert, prob))
+        return prob
 
-            prob = math.exp(- self._alpha * norm_nvis)
-            if walk == cur_walk: # ignore current walk
-                pr_w = prob
-            acc += prob
-
-        assert pr_w != 0.0
-        pr_w = pr_w / acc
-        print('\t\t__exp: w{}, v{}={}'.format(cur_walk, vert, pr_w))
-        return pr_w
-
-    def __pow(self, cur_walk: int, vert: int):
+    def __pow(self, walks: Walks, cur_walk: int, vert: int):
         """Apply a factor to a power function of the number of visits and the
         reinforcing factor alpha.
 
         """
-        factor = self.count_visits(cur_walk, vert)
+        # Total number of visits in the vertex vert
+        total_nvis = vertex_count_visits(walks, vert)
 
-        return factor * \
-            pow(self._m - self.sum_count_visits(cur_walk, vert),
-                self._alpha)
+        # Normalized number of visits of the current walk.
+        norm_nvis_cur_walk = \
+            walks.get(cur_walk).nvisits(vert) / total_nvis
 
-    def calc_prob(self, cur_walk, v_src, v_dest):
+        acc = \
+            sum_norm_vertex_visits_from_other_walks(walks, cur_walk, vert)
+
+        prob = norm_nvis_cur_walk * pow(len(walks) - acc, self._alpha)
+
+        # TODO: put print() into log file
+        #print('\t\tPOW: w{}, v{}={}'.format(cur_walk, vert, prob))
+        return prob
+
+    def calculate(self, walks: Walks, cur_walk: int, \
+                  v_dest: int) -> float:
         """Calculate the transition probability of the current walk that are
         located at source vertex to go to destination vertex.
 
+        walks (Walks): Walks object to access data
+        from each walk
         cur_walk (Walk): current walk
         v_src (int): source vertex
         v_dest (int): destination vertex
 
         """
-        # The values from the time step before the current
-        # are used to calculate the probability.
-        pr_v = 0.0
-        pr_sum = 0.0
-
-        for vert in self._graph.neighbors(v_src):
-            prob = self._func(cur_walk, vert)
-            pr_sum += prob
-
-            if vert == v_dest:
-                pr_v = prob
-
-        return pr_v/pr_sum
-
-    def calc(self, walk: int, v_src: int, v_dest: int):
-        """Calculate the probability of the walk to go from vertex v_src to
-        the vertex v_dest.
-
-        rrwg (RRWG): RRWG object
-        walk (int): walk index
-        v_src (int): vertex source index
-        v_dest (int): vertex destination index
-
-        """
-        return self._func()
+        return self.__func(walks, cur_walk, v_dest)
