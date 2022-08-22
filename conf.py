2136ed66f94031470b17c539f12ae047931224bc
Adriano J. Holanda <aholanda@usp.br>
2022-08-22 17:30:56 -0300

    test configuration file existence

--- conf.py
+++ conf.py
@@ -2,6 +2,16 @@
 
 """
 import configparser
+import os
+import sys
+
+FILENAME = 'rrwg.conf'
 
 config = configparser.ConfigParser()
-config.read('rrwg.conf')
+if os.path.exists(FILENAME):
+    config.read(FILENAME)
+else:
+    print('Please fill up a configuration file called "{}".'
+          .format(FILENAME))
+    print('See https://github.com/aholanda/rrwg/blob/main/rrwg.conf for an example.')
+    sys.exit(-1)
